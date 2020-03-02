484b6a4abd8c04a48979e26badd40e88436ae24e
Irina Petrova <iraloseva@bk.ru>
2020-03-02 22:57:10 +0300

    initial commit

--- W1/src/main/java/ru/otus/spring/petrova/dao/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.petrova.dao;
-
-import ru.otus.spring.petrova.domain.Question;
-
-import java.util.List;
-
-public interface QuestionDao {
-  List<Question> getQuestions();
-}
--- W1/src/main/java/ru/otus/spring/petrova/domain/LoadQuestionError.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.petrova.domain;
-
-public class LoadQuestionError extends Error {
-  public LoadQuestionError(String message) {
-    super(message);
-  }
-}
