e46f799f7eec9b79a42dbb3cd9fe81887f6121f3
Irina Petrova <iraloseva@bk.ru>
2020-03-15 13:15:20 +0300

    L4: delete aspect

--- L4/src/main/java/ru/otus/spring/petrova/Main.java
+++ L4/src/main/java/ru/otus/spring/petrova/Main.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import ru.otus.spring.petrova.config.YamlProps;
 import ru.otus.spring.petrova.service.TestingService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(YamlProps.class)
-@EnableAspectJAutoProxy
 public class Main {
   public static void main(String[] str) {
     ConfigurableApplicationContext context = SpringApplication.run(Main.class, str);
